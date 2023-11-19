0efadef962b0dce112dfda647c376548b7612981
Seonghyeon Kim <why111922@gmail.com>
2023-11-19 23:06:56 +0900

    2023.11.18.practice_c

---
+++ practice_1118_1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+#define Name_len 10
+
+typedef struct student
+struct student
+{
+	char name[Name_len];
+	char phone[20];
+	int score;
+}STU;
+
+int main(void)
+{
+	STU s1;
+	
+}
+
